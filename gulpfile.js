45eb5befdbb936280b8ba88f1ebbe06801993092
Victor <victor.dupuy@hei.fr>
2015-12-23 11:54:08 +0100

    Adding Error handling in automation process (Gulp plumber)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var plumber = require('gulp-plumber');
 
 //Defining the task to transform SCSS into CSS
 gulp.task('styles', function(){
   gulp.src('public/sass/**/*.scss')
+    .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css/'));
 });
 
 gulp.task('babel', function(){
   return gulp.src('jsDev/*.js')
+    .pipe(plumber())
 		.pipe(babel({
 			presets: ['es2015']
 		}))
